d998397f5c393a22ac1a895d5d895dab08b55f24
akshay-1231 <akshayprajapati424206@gmail.com>
2024-09-19 19:53:23 +0530

    Update question1final.cpp

--- question1final.cpp
+++ question1final.cpp
@@ -1,54 +1,34 @@
 #include<iostream>
 using namespace std;
 void add_book(string title, string author,int ISBN,int price,int quantity){
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+creat filefolder(){
+    ofstream out;
+	out.open(file+".txt",ios::app);
+    cout<<"entre name of file";
+	cin>>file;
+	getline(cin,str);
+	out<<str;
+	while(in.get(c))	{
 		
-}
-
-main(){
-	cout<<"\n1. Add Book Records";
-	cout<<"\n2. Show Book Records";
-	cout<<"\n3. Check Book Records";
-	cout<<"\n4. Modify Book Records";
-	cout<<"\n5. Delete Book Records";
-	cout<<"\n6. Exit";
-	int ch;
-	cout<<"\nEnter the choice: ";
-	cin>>ch;
-	switch (ch){
-		case 1:
-			string title,author;
-			int ISBN,price,quantity;
-			cout<<"\nEnter Title:";
-			getline(cin,title);
-			cout<<"\nEnter name of author:";
-			getline(cin,author);
-			cout<<"\nEnter  ISBN number:";
-			cin>>ISBN;
-			cout<<"\nEnter  price of Book:";
-			cin>>price;
-			cout<<"\nEnter  Quantity:";
-			cin>>quantity;
-			add_book(title,author,ISBN,price,quantity);
-			break;
-			case 2:
-			show_book();
-		break;
-		case 3:
-			check_book();
-			break;
-		case 4:		
-			modify_book();
-
-			break;
-	case 5:
-		Delete_book();
-		break;
-	case 6:
-		cout<<"\nThank you for ...";
-		break;
-		default:
-			cout<<"\ please try again..";
-			
+		if(str=='\0')
+		    break;
+		    else 
+		    out<<str;	  	
+		
 	}
+	
+} 
+out.close();
 
+display filefolder(){
+	ifstream in;
+	in.open(file+".txt");
+	cout<<"entre data in the file";
+	cin>>file;
+	
+	
 }
